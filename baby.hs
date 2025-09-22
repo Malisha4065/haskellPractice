c33ffec7881ae7e4ac371e06a7a36099ea592fa3
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2025-09-22 15:18:38 +0530

    lists

--- baby.hs
+++ baby.hs
@@ -3,3 +3,11 @@ doubleMe x = x + x
 -- doubleUs x y = x * 2 + y * 2
 -- or simply...
 doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+                        then x
+                        else x * 2
+
+doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1   -- ' is valid for function names
+
+-- Nope x = x -> function names do not start with Capital letters
---
+++ list.hs
@@ -0,0 +1,13 @@
+lostNumbers = [4, 8, 15, 16, 23, 42]
+
+concatList = [1, 2, 4] ++ [3, 5, 6]     -- [1,2,4,3,5,6]
+
+hi = "Hello " ++ "world!"               -- "Hello world!"
+
+hello = ['H', 'e', 'l', 'l', 'o']       -- "Hello"
+
+longH = hi ++ " " ++ hello              -- "Hello world! Hello"
+
+woot = ['w', 'o'] ++ ['o', 't']         -- "woot"
+
+cat = 'A' : " SMALL CAT"                -- INSTANT faster than ++ operation "A SMALL CAT"
